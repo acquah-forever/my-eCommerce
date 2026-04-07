35a283192ac4e4610c794d65624e1a17d2f76b25
acquah-forever <kojoaddo598@gmail.com>
2026-04-07 22:29:09 +0000

    centered images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,13 @@ const Home = () => {
   return (
     <div className='flex flex-col relative bg-[url("https://cdn.pixabay.com/photo/2024/01/29/14/40/warehouse-8540045_1280.jpg")] bg-cover bg-center bg-fixed bg-no-repeat'>
       <div className='absolute inset-0 bg-black/30'></div>
-      <div className='relative z-50'>
-        <div className='flex flex-col justify-center items-center space-y-5'>
+      <div className='relative z-50 py-20'>
+        <div className='flex flex-col justify-center items-center space-y-5 text-white'>
           <h1 className='text-3xl font-semibold'>Welcome to Shophub</h1>
-          <p className='text-sm'>Discover Amaxing Products at Greate Prices</p>
+          <p className='text-xl'>Discover Amaxing Products at Greate Prices</p>
         </div>
         <div className='p-5 mb-5'>
-          <h2 className='font-semibold text-2xl'>Our Products</h2>
+          <h2 className='font-semibold text-2xl text-white'>Our Products</h2>
         </div>
         <div className='grid space-x-7 space-y-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
           {products.map((product) => (
